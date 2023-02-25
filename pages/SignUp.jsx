eae241505abef9c4b5e6677ab52d07d2b6f7753a
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2023-02-25 18:12:12 +0100

    testing chakra

--- pages/SignUp.jsx
+++ pages/SignUp.jsx
@@ -1,7 +1,11 @@
+import { Text } from "@chakra-ui/react";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return <div>
+    <Text
+    fontSize="4xl">Sign Up</Text>
+  </div>;
 };
 
 export default SignUp;
